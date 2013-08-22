66629fb6596c0ebad221e6c6bb3984c321c455b5
cesine <cesine@yahoo.com>
2013-08-22 12:07:10 -0400

    formatting

--- templates/datalist_template.js
+++ templates/datalist_template.js
@@ -1,11 +1,11 @@
 module.exports = {
-   "title": "Default Datalist - Empty",
-   "description": "The app comes with a default datalist which is empty. Once you have data in your corpus, you can create a datalist using Search. Imported data will also show up as a datalist. Datalists can be used to create handouts, export to LaTeX, or share with collaborators.",
-   "datumIds": [],
-   "pouchname": "testingcorpustemplates-firstcorpus",
-   "comments": [],
-   "dateCreated": "\"2013-08-01T14:54:18.385Z\"",
-   "dateModified": "\"2013-08-01T15:41:27.189Z\"",
-   "collection": "datalists",
-   "timestamp": 1375371687189
+	"title": "Default Datalist - Empty",
+	"description": "The app comes with a default datalist which is empty. Once you have data in your corpus, you can create a datalist using search. Imported data will also show up as a datalist. Datalists are lists of data which can be used to create handouts, export to LaTeX, or share with collaborators.",
+	"datumIds": [],
+	"pouchname": "testingcorpustemplates-firstcorpus",
+	"comments": [],
+	"dateCreated": "\"2013-08-01T14:54:18.385Z\"",
+	"dateModified": "\"2013-08-01T15:41:27.189Z\"",
+	"collection": "datalists",
+	"timestamp": 1375371687189
 };
