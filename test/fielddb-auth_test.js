d8a02a80762c15f578808550bf8ecc1ba0a90c36
cesine <cesine@yahoo.com>
2015-02-20 19:28:41 +0700

    added a json request to test the api, but decided to go for curl instead

--- test/fielddb-auth_test.js
+++ test/fielddb-auth_test.js
@@ -1,6 +1,47 @@
 'use strict';
 
+var http = require('http'),
+  https = require('https');
+// https://github.com/Zugwalt/nodeunit-async
+// 
 var palindrode = require('../lib/fielddb-auth.js');
+
+var makeJSONRequest = function(options, data, onResult) {
+
+  var httpOrHttps = http;
+  if (options.protocol == 'https://') {
+    httpOrHttps = https;
+  }
+  delete options.protocol;
+
+  var req = httpOrHttps.request(options, function(res) {
+    var output = '';
+    res.setEncoding('utf8');
+
+    res.on('data', function(chunk) {
+      output += chunk;
+    });
+
+    res.on('end', function() {
+      var obj = JSON.parse(output);
+      onResult(res.statusCode, obj);
+    });
+  });
+
+  req.on('error', function(err) {
+    console.log('Error searching for ' + JSON.stringify(data));
+    console.log(options);
+    console.log(err);
+  });
+
+  if (data) {
+    req.write(data, 'utf8');
+    req.end();
+  } else {
+    req.end();
+  }
+
+};
 
 /*
   ======== A Handy Little Nodeunit Reference ========
